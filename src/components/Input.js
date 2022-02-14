de58d9c4e1111d4c59ab41bf3632110abca9e3c1
DarkedgeTH <darkedgeth@gmail.com>
2022-02-14 14:22:30 -0500

    completed steps 1 and 2 of input

--- src/components/Input.js
+++ src/components/Input.js
@@ -38,6 +38,7 @@ import React, {useState} from 'react'; /* STEP 0 */
 
 export default function Input() {
   /* STEP 1 */
+  const [inputValue, setInputValue] = useState('');
 
   const changeInput = evt => {
     // When the input changes, its whole value can be found inside the event object.
@@ -53,7 +54,7 @@ export default function Input() {
   const style = {
     fontSize: '1.5em',
     marginBottom: '0.3em',
-    color: 'royalblue', /* STEP 2 */
+    color: inputValue.length > 10 ? 'crimson' : 'royalblue' /* STEP 2 */
   };
 
   return (
